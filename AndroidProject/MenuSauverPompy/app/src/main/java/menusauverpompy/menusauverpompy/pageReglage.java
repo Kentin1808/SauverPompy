59ac27b01fc2c2e0e77ce4d0a64fd86969b36d52
WrsAce <34317173+WrsAce@users.noreply.github.com>
2018-11-21 18:54:44 +0100

    Menu
    Voici le Menu

---
+++ AndroidProject/MenuSauverPompy/app/src/main/java/menusauverpompy/menusauverpompy/pageReglage.java
@@ -0,0 +1,16 @@
+package menusauverpompy.menusauverpompy;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class pageReglage extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reglage);
+    }
+}
