c3e41ecc0736131b060986ac5c084c3cc32865c2
kentin <quentin.buttard@etu.univ-lyon1.fr>
2018-12-04 20:23:16 +0100

    Test de différentes facons pour régler les pb d'affichage
    L'affichage de la page sur android studio ne correspond PAS DU TOUT à l'affichage sur telephone ! ===> Incompréhension

---
+++ AndroidProject/Cours/SauvezPompyV2/app/src/main/java/android/sauvezpompy/sauvezpompy/Cours_Brulures.java
@@ -0,0 +1,13 @@
+package android.sauvezpompy.sauvezpompy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cours_Brulures extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cours_brulures);
+    }
+}
